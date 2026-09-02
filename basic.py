228a7272ce0b9456ed9adeb5b929bba8f3979042
Jayesh-Gi <mrjayesh.0044@gmail.com>
2026-09-02 16:00:17 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,7 +1,7 @@
-a=int(input("ENTER FIRST NUMBER: "))
-B= int(input("ENTER SECOND NUMBER: "))
-
-if a > B :
-    print("FIRST NUMBER IS GREATER THAN SECOND NUMBER")
-else:
-    print("SECOND NUMBER IS GREATER THAN FIRST NUMBER")  
+write table of 5Here is the table of 5:5 x 1 = 5
+5 x 2 = 10  5 x 3 = 15
+5 x 4 = 20   5 x 5 = 25
+5 x 6 = 30   5 x 7 = 35 5 x 8 = 40   5 x 9 = 45
+5 x 10 = 50 
+print("Here is the table of 5:")
+for i in range(1, 11):  
